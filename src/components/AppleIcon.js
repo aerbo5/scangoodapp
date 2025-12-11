a91eceb11771cd59928b3d5ecb578d037f2408f3
aerbo5 <aerbo5@users.noreply.github.com>
2025-12-11 12:58:43 -0500

    Fix Apple icon: make it white on black button background

--- src/components/AppleIcon.js
+++ src/components/AppleIcon.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Image, StyleSheet } from 'react-native';
+import { Colors } from '../constants';
 
 // Apple logo - using official Apple logo
-const AppleIcon = ({ size = 24, color = '#000000' }) => {
+// For dark backgrounds (black button), use white logo
+const AppleIcon = ({ size = 24, color = '#FFFFFF' }) => {
+  // Use white Apple logo for dark backgrounds (black button)
+  // For light backgrounds, use black logo
+  const isWhite = color === '#FFFFFF' || color === Colors?.white || color === 'white';
+  
   return (
     <Image
       source={{
-        uri: 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg'
+        uri: isWhite 
+          ? 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg' // Will be tinted white
+          : 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg'
       }}
-      style={[styles.logo, { width: size, height: size }]}
+      style={[
+        styles.logo, 
+        { 
+          width: size, 
+          height: size,
+          tintColor: isWhite ? '#FFFFFF' : undefined // Tint to white for dark backgrounds
+        }
+      ]}
       resizeMode="contain"
     />
   );
